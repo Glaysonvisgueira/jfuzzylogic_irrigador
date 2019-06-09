83c0fa2bd46da28b985c0649a083c52bdfe3ec03
Glaysonvisgueira <31600079+Glaysonvisgueira@users.noreply.github.com>
2019-06-08 22:35:31 -0300

    Rename Main.java to ProgramaPrincipal.java

--- ProgramaPrincipal.java
+++ ProgramaPrincipal.java
@@ -14,39 +14,39 @@ public class ProgramaPrincipal {
 		
 		JFuzzyChart.get().chart(fis);
 		
-		fb.setVariable("temperatura",35);    //Escala: 0 à 50
-		fb.setVariable("umidade", 4);		   //Escala: 0 à 4.2
-		fb.setVariable("luminosidade",800);   //Escala: 0 à 1023        
+		fb.setVariable("temperatura",35);    //Escala: 0 Ã  50
+		fb.setVariable("umidade", 4);		   //Escala: 0 Ã  4.2
+		fb.setVariable("luminosidade",800);   //Escala: 0 Ã  1023        
         
-        fis.evaluate(); //Utilização da inferência        
+        fis.evaluate(); //UtilizaÃ§Ã£o da inferÃªncia        
 
         Variable controladorIrrigacao = fb.getVariable("potenciaIrrigacao");
         JFuzzyChart.get().chart(controladorIrrigacao,controladorIrrigacao.getDefuzzifier(), true);
 
         double valor = fb.getVariable("potenciaIrrigacao").getValue();
-        System.out.println("VALORES DE REFERÊNCIA:\n");
-        System.out.println("DESLIGADO:  0 À 10");
-        System.out.println("FRACA....: 11 À 16");
-        System.out.println("MÉDIA....: 17 À 25");
-        System.out.println("FORTE....: 26 À 30\n");
+        System.out.println("VALORES DE REFERÃŠNCIA:\n");
+        System.out.println("DESLIGADO:  0 Ã€ 10");
+        System.out.println("FRACA....: 11 Ã€ 16");
+        System.out.println("MÃ‰DIA....: 17 Ã€ 25");
+        System.out.println("FORTE....: 26 Ã€ 30\n");
         System.out.println("---------------------------");
-        System.out.printf("\nVALOR DE POTÊNCIA: %.2f\n", fb.getVariable("potenciaIrrigacao").getValue());
+        System.out.printf("\nVALOR DE POTÃŠNCIA: %.2f\n", fb.getVariable("potenciaIrrigacao").getValue());
         
         if(valor <= 10) {
         	
-        	System.out.println("POTÊNCIA DO IRRIGADOR: DESLIGADO");
+        	System.out.println("POTÃŠNCIA DO IRRIGADOR: DESLIGADO");
         	
         }else if(valor <= 16){    
         	
-        	System.out.println("POTÊNCIA DO IRRIGADOR: FRACA");
+        	System.out.println("POTÃŠNCIA DO IRRIGADOR: FRACA");
         	
         }else if(valor <= 25){
         	
-        	System.out.println("POTÊNCIA DO IRRIGADOR: MÉDIA");
+        	System.out.println("POTÃŠNCIA DO IRRIGADOR: MÃ‰DIA");
         	
         }else if(valor > 25){
         	
-        	System.out.println("POTÊNCIA DO IRRIGADOR: FORTE");
+        	System.out.println("POTÃŠNCIA DO IRRIGADOR: FORTE");
         
         }
 	}
